fb9d9ac6cf24cb9852b48dc34affc786d9c31a44
TutEvgen1984 <35139330+TutEvgen1984@users.noreply.github.com>
2025-12-12 23:42:09 +0700

    12.12.2025

---
+++ phpLessons/lesson1.1/public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php - урок 1</title>
+</head>
+<body>
+    <h1>Урок 1.1</h1>
+    <?php echo "My site с сервером OSP"?>
+</body>
+</html>
+
---
+++ phpLessons/lesson1.2/public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "php - урок 1.2"?></title>
+</head>
+<body>
+    <?php
+    echo date(DATE_RSS);
+    ?>
+</body>
+</html>
+
