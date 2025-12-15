d0c52b0a3022b17e2682a18a2567a7640bd0c6e0
TutEvgen1984 <tutevgen1984@ya.ru.>
2025-12-15 16:23:05 +0700

    15.12.2025

---
+++ phpLessons/lesson1.4/public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?= "php - урок 1.4"?>
+    </title>
+    <style>
+        body {color: white; background-color: black;}
+    </style>
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
