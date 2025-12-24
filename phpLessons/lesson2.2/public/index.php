5379a499ff8c1b6bcc9128f371494117359e9ce2
TutEvgen1984 <tutevgen1984@ya.ru.>
2025-12-24 16:26:48 +0700

    24.12.2025

--- phpLessons/lesson2.2/public/index.php
+++ phpLessons/lesson2.2/public/index.php
@@ -26,15 +26,40 @@
 
     echo "<br>";
 
-    $first = $second = 2;
+    // рассмотрим переменные:
+    $first = 2;
+    $second = $first;
 
     // переопределим переменную
     $first = 3;
 
-    echo $first . "<br>";
-    // 3
-    echo $second . "<br>";
-    // 2
+    echo "{$first}<br>";
+    // 3 (стала 3 вместо 2)
+    echo "{$second}<br>";
+    // 2 (осталась изначальной)
+
+    $first_obj = new Point;
+    $first_obj->x = 3;
+    $first_obj->y = 3;
+
+    // второй объект ссылается на первый (это не клонирование)
+    $second_obj = $first_obj;
+    $second_obj->x = 5;
+    $second_obj->y = 5;
+
+    echo "first_obj. x: {$first_obj->x}, y: {$first_obj->y} <br>";
+    // first_obj. x: 5, y: 5
+
+    // переменные, способы передачи ссылками
+    $first_var = 5;
+
+    // с помощью & задаётся указание на создание ссылки
+    $second_var = &$first_var;
+    $second_var = 3;
+
+    echo "first_var=$first_var, second_var=$second_var";
+    // first_var=3, second_var=3
+
 
 
     ?>
