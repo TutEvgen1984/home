07fa5c016d5796a1c71c3efab4e6ebfc73067872
TutEvgen1984 <tutevgen1984@ya.ru.>
2026-01-29 16:17:25 +0700

    29.01.2026

---
+++ phpLessons/lesson6.2/public/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?= "php - урок 6.2" ?>
+    </title>
+    <style>
+        body {
+            color: white;
+            background-color: black;
+            font-size: 36px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <?php
+    // 6.2 Работа с подстроками
+    // https://www.php.net/manual/ru/function.substr.php
+    // https://www.php.net/manual/ru/function.strpos.php
+    // https://www.php.net/manual/ru/function.str-replace.php
+    // https://www.php.net/manual/ru/function.trim.php
+
+
+
+    ?>
+
+</body>
+
+</html>
