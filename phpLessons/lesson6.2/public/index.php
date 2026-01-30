809314a6773785af22f2a6c48d27cd36de5fa465
TutEvgen1984 <tutevgen1984@ya.ru.>
2026-01-30 16:26:37 +0700

    30.01.2026

--- phpLessons/lesson6.2/public/index.php
+++ phpLessons/lesson6.2/public/index.php
@@ -20,12 +20,51 @@
 
     <?php
     // 6.2 Работа с подстроками
-    // https://www.php.net/manual/ru/function.substr.php
-    // https://www.php.net/manual/ru/function.strpos.php
-    // https://www.php.net/manual/ru/function.str-replace.php
     // https://www.php.net/manual/ru/function.trim.php
 
+    $date = '30.01.2026';
 
+    // substr — Возвращает подстроку
+    // https://www.php.net/manual/ru/function.substr.php
+
+    echo 'день ' . substr($date, 0, 2) . '<br>';
+    echo 'месяц ' . substr($date, 3, 2) . '<br>';
+    echo 'год ' . substr($date, 6) . '<br>';
+    // день 30
+    // месяц 01
+    // год 2026
+
+    // ***********
+
+    // strpos — string position - Ищет позицию первого вхождения подстроки
+    // https://www.php.net/manual/ru/function.strpos.php
+
+    $str1 = 'Hello world';
+    echo strpos($str1, 'world');
+    echo "<br>";
+    // 6
+
+    $str2 = 'PHP - this интерпретируемый язык';
+    echo strpos($str2, 'интер');
+    echo "<br>";
+    // 11 (если искать в строке с кириллицей, то номера получается завышенные, но все равно  дальнейший поиск работает)
+    echo substr($str2, strpos($str2, 'интер'));
+    // интерпретируемый язык
+
+    echo "<br>";
+
+    // *******************
+    // замена текста
+    // https://www.php.net/manual/ru/function.str-replace.php
+
+    $str3 = 'PHP - [b]интерпретируемый[/b] язык, а это [b]жирный текст[/b]';
+    $str3 = str_replace('[b]', '<b>', $str3);
+    echo $str3 . "<br>";
+    // PHP - интерпретируемый[/b] язык, а это жирный текст[/b] (текст на странице жирный начиная с тире)
+
+    $str3 = str_replace('[/b]', '</b>', $str3);
+    echo $str3 . "<br>";
+    // PHP - интерпретируемый язык, а это жирный текст (весь текст жирный)
 
     ?>
 
