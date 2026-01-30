14493208c489f88b2bc06771c6548df564bf54fc
TutEvgen1984 <35139330+TutEvgen1984@users.noreply.github.com>
2026-01-30 22:40:34 +0700

    апд

---
+++ phpLessons/lesson6.3/public/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?= "php - урок 6.3" ?>
+    </title>
+    <style>
+        body {
+            color: white;
+            background-color: black;
+            font-size: 36px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <?php
+    // 6.3 Функции для работы с HTML
+
+    ?>
+
+</body>
+
+</html>
