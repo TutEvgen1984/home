2c7bab04601791c6547c311fa258702b60d702e5
TutEvgen1984 <35139330+TutEvgen1984@users.noreply.github.com>
2026-02-02 21:44:03 +0700

    апд

--- phpLessons/lesson6.5_tasks/public/index.php
+++ phpLessons/lesson6.5_tasks/public/index.php
@@ -52,14 +52,55 @@
 
     echo "Количество символов в название города " . isEven($nSymbols2), "<br>";
     // Количество символов в название города Не четное
-    echo "<br>";
 
+    echo "<br>";
 
     // *******************************
 
     // 6.3. Даны две фамилии. Определить, какая из них длиннее.
 
+    $secondName31 = "Тютиков";
+    $secondName32 = "Черемных";
 
+    $nSymbols31 = mb_strlen($secondName31);
+    echo "nSymbols31 = ", $nSymbols31, "<br>";
+
+    $nSymbols32 = mb_strlen($secondName32);
+    echo "nSymbols32 = ", $nSymbols32, "<br>";
+
+    if ($nSymbols31 > $nSymbols32) {
+        echo "Фамилия №1 длиннее чем фамилия №2", "<br>";
+    } elseif ($nSymbols31 == $nSymbols32) {
+        echo "Фамилия №1 равна длине фамилии №2", "<br>";
+    } else {
+        echo "Фамилия №2 длиннее чем фамилия №1", "<br>";
+    }
+
+    echo "<br>";
+
+    // ***********************
+
+    // 6.4. Даны названия трех городов. Вывести на экран самое длинное и самое короткое названия.
+
+    $citiesNames4 = ["Томск", "Омск", "Барнаул"];
+
+    $citiesLengths4 = [mb_strlen($citiesNames4[0]), mb_strlen($citiesNames4[1]), mb_strlen($citiesNames4[2])];
+
+    echo "citiesLengths4 = ";
+    print_r($citiesLengths4);
+    echo "<br>";
+    // citiesLengths4 = Array ( [0] => 5 [1] => 4 [2] => 7 )
+
+    // поиск самого длинного названия
+    $maxIndex4 = 0;
+    $minIndex4 = 0;
+    for ($i = 0; $i < count($citiesNames4); $i++) {
+        if (mb_strlen($citiesNames4[$i])) {
+        }
+    }
+
+    $max4 = max($citiesLengths4);
+    echo $max4, "<br>";
 
     ?>
 
