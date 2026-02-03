7e092171d7ffe4dad2c4999be2fcf84d641c1563
TutEvgen1984 <tutevgen1984@ya.ru.>
2026-02-03 16:17:27 +0700

    03.02.2026

--- phpLessons/lesson6.5_tasks/public/index.php
+++ phpLessons/lesson6.5_tasks/public/index.php
@@ -91,16 +91,79 @@
     echo "<br>";
     // citiesLengths4 = Array ( [0] => 5 [1] => 4 [2] => 7 )
 
-    // поиск самого длинного названия
-    $maxIndex4 = 0;
-    $minIndex4 = 0;
-    for ($i = 0; $i < count($citiesNames4); $i++) {
-        if (mb_strlen($citiesNames4[$i])) {
+    $maxLength4 = max($citiesLengths4);
+    echo $maxLength4, "<br>";
+    // 7
+
+    $minLength4 = min($citiesLengths4);
+    echo $minLength4, "<br>";
+    // 4
+
+    // поиск индекса по значению (максимального и минимального):
+    // array_search — Ищет значение в массиве, и если находит, возвращает ключ первого найденного элемента
+    // https://www.php.net/manual/ru/function.array-search.php
+
+    $maxIndex4 = array_search($maxLength4, $citiesLengths4);
+    echo "maxIndex4=", $maxIndex4, "<br>";
+    // maxIndex4=2
+
+    $minIndex4 = array_search($minLength4, $citiesLengths4);
+    echo "minIndex4=", $minIndex4, "<br>";
+    // minIndex4=1
+
+    // поиск макс и мин значения по найденным индексам:
+
+    // https://www.php.net/manual/ru/function.printf.php
+
+    $maxValue4 = $citiesNames4[$maxIndex4];
+    printf("Самое длинное название: %s <br>", $maxValue4);
+    // Самое длинное название: Барнаул
+
+    $minValue4 = $citiesNames4[$minIndex4];
+    printf("Самое короткое название: %s <br>", $minValue4);
+    // Самое короткое название: Омск
+
+    // ************************
+    // 6.5. Дано слово. Вывести на экран его третий символ.
+
+    // слово на английском
+    $word5_1 = "word";
+    $symbol3_51 = $word5_1[2];
+    printf("Третий символ: %s <br>", $symbol3_51);
+    // Третий символ: r
+
+    // слово на русском
+    $word5_2 = "Слово";
+    $symbol3_52 = mb_substr($word5_2, 2, 1);
+    printf("Третий символ: %s <br>", $symbol3_52);
+    // Третий символ: r
+
+    // *********************
+
+    // 6.6. Дано слово. Вывести на экран его последний символ.
+    $word6 = "Слово2 Терминатор";
+    $symbolLast_6 = mb_substr($word6, -1, 1);
+    printf("Последний символ: %s <br>", $symbolLast_6);
+
+    // 6.7. Дано предложение. Вывести все буквы м и н в нем.
+    $str6 = "Терминатор и робокоп ннн";
+    echo mb_strlen($str6), '<br>';
+    // 20
+
+    echo $str6[0], '<br>';
+    // � (потому что русские буквы, не прокатит, надо использовать mb_substr)
+
+    for ($i = 0; $i < mb_strlen($str6); $i++) {
+        if ((mb_substr($str6, $i, 1) == 'м') or (mb_substr($str6, $i, 1) == 'н')) {
+            echo mb_substr($str6, $i, 1) . "<br>";
         }
     }
+    // м
+    // н
+    // н
+    // н
+    // н
 
-    $max4 = max($citiesLengths4);
-    echo $max4, "<br>";
 
     ?>
 
---
+++ phpLessons/lesson7.1/public/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?= "php - урок 7.1" ?>
+    </title>
+    <style>
+        body {
+            color: white;
+            background-color: black;
+            font-size: 36px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <?php
+    // 7.1 Передача параметров методом GET
+
+
+
+    ?>
+
+</body>
+
+</html>
