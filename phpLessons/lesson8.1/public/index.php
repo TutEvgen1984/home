5f33e5bbda38db3f4b43e698b8f7c62317acfe8d
TutEvgen1984 <tutevgen1984@ya.ru.>
2026-02-09 16:20:43 +0700

    09.02.2026

--- phpLessons/lesson8.1/public/index.php
+++ phpLessons/lesson8.1/public/index.php
@@ -5,51 +5,88 @@
     <meta charset="UTF-8">
     <meta name="viewport" content='width=device-width, initial-scale=1.0'>
     <title>Урок 52</title>
+    <style>
+        body {
+            color: white;
+            background-color: black;
+            font-size: 36px;
+        }
+    </style>
 </head>
 
 <body>
-    <!-- 8.1 Методы класса -->
 
     <?php
-    // class Hello {
-    //     public function printText() {
-    //         return "Привет, мир!";
-    //     }
-    // }
-    // $obj = new Hello;
-    // echo $obj->printText();
 
-    // class Point {
-    //     private $x;
-    //     private $y;
-    //     public function setX($x) {
-    //         $this->x = $x;
-    //     }
-    //     public function setY($y) {
-    //         $this->y = $y;
-    //     }
-    //     public function getX() {
-    //         return $this->x;
-    //     }
-    //     public function getY() {
-    //         return $this->y;
-    //     }
-    //     public function distance() {
-    //         return sqrt($this->getX()**2 + $this->getY()**2);
-    //     }
-    // }
-    // $p1 = new Point;
-    // $p1->setX(2);
-    // $p1->setY(3);
-    // echo $p1->distance();
+    // 8.1 Методы класса
 
-    // class Hello {
-    //     public static function printText() {
-    //         return "Привет, мир!";
-    //     }
-    // }
-    // echo Hello::printText();
+    class Hello
+    {
+        // функция внутри класса называется методом
+        // спецификатор public чтобы был везде доступ к этой функции
+        public function printText()
+        {
+            return "Привет, мир!";
+        }
+    }
+    // создадим объект на основе класса. При этом функция внутри класса сама по себе не выполнится
+    $obj = new Hello;
 
+    // чтобы выполнить публичный метод объекта нужно к нему обратиться так же как к переменным объекта с помощью ->
+    echo $obj->printText() . "<br>";
+    // Привет, мир!
+
+    class Point
+    {
+        // переменные $x и $y с закрытым доступом
+        private $x;
+        private $y;
+
+        // функции с открытым доступом снаружи
+        public function setX($x)
+        {
+            $this->x = $x;
+        }
+        public function setY($y)
+        {
+            $this->y = $y;
+        }
+        public function getX()
+        {
+            return $this->x;
+        }
+        public function getY()
+        {
+            return $this->y;
+        }
+        public function distance()
+        {
+            return sqrt($this->getX() ** 2 + $this->getY() ** 2);
+        }
+    }
+    $p1 = new Point;
+    $p1->setX(2);
+    $p1->setY(3);
+    echo $p1->distance() . "<br>";
+    // 3.605551275464
+
+    // **********************
+
+    // статический метод (объект класса теперь сам создавать не обязательно):
+    // https://www.php.net/manual/ru/language.oop5.static.php
+    class Hello2
+    {
+        public static function printText()
+        {
+            return "Привет, мир2! <br>";
+        }
+    }
+    echo Hello2::printText();
+    // Привет, мир2!
+
+    // ********************
+
+    // ключевое слово self, которое означает название класса. Сейчас класс называется Page, но если оно изменится например на Sage, то self теперь будет указывать на Sage
     class Page
     {
         static $content = 'Тело сайта<br>';
@@ -70,6 +107,10 @@
     }
 
     Page::site();
+    // Это шапка сайта
+    // Тело сайта
+    // Это подвал сайта
+
     ?>
 </body>
 
