a6783080e48ff599ba5949703c1785484ab69f77
TutEvgen1984 <tutevgen1984@ya.ru.>
2026-05-29 15:59:23 +0700

    апд

---
+++ landing-bw_wp/public/wp-content/themes/bw-theme/functions.php
@@ -0,0 +1,24 @@
+<?php
+add_action('wp_enqueue_scripts', 'add_scripts_and_styles');
+
+function add_scripts_and_styles()
+{
+    // подключение своего jquery взамен встроенного в wp
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', get_template_directory_uri() . '/assets/js/jquery-3.5.1.min.js', false, null, true);
+    wp_enqueue_script('jquery');
+
+    // подключение своего main.js после jquery
+    wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), null, 'footer');
+
+    wp_enqueue_style('fontello', get_template_directory_uri() . '/assets/css/fontello.css');
+    // на странице появилась:
+    // <link rel="stylesheet" id="fontello-css" href="https://landingbwwp/wp-content/themes/bw-theme/assets/css/fontello.css?ver=7.0" media="all">
+
+    // подключение style.css
+    wp_enqueue_style('main', get_stylesheet_uri(), array('fontello'));
+    // с условием подключения после fontello.css
+    // на странице появилась:
+    // <link rel="stylesheet" id="main-css" href="https://landingbwwp/wp-content/themes/bw-theme/style.css?ver=7.0" media="all">
+
+}
