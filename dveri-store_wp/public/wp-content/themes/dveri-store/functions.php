790ffed1aea35947909ddcbe16bf01efbcaf1264
TutEvgen1984 <tutevgen1984@ya.ru.>
2026-07-02 15:43:23 +0700

    02.07.2026

--- dveri-store_wp/public/wp-content/themes/dveri-store/functions.php
+++ dveri-store_wp/public/wp-content/themes/dveri-store/functions.php
@@ -7,3 +7,16 @@ function add_scripts_and_styles()
     // Подключаем style.css который лежит в корне темы
     wp_enqueue_style('style', get_stylesheet_uri());
 }
+
+// разблокировка возможности выбора своего собственного логотипа в вордпрессе
+// https://wp-kama.ru/function/add_theme_support
+add_action('after_setup_theme', 'add_features');
+
+function add_features()
+{
+    add_theme_support('custom-logo', array(
+        'height' => 50,
+        'width'  => 100,
+    ));
+}
+// внешний вид => настроить => свойства сайта => выбрать логотип
--- dveri-store_wp/public/wp-content/themes/dveri-store/header.php
+++ dveri-store_wp/public/wp-content/themes/dveri-store/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <!-- <link rel="shortcut icon" href="img/logo.png" type="image/x-icon"> -->
     <link rel="stylesheet" href="css/style.css">
     <title>Главная</title>
 
@@ -19,8 +19,11 @@
         <div class="container">
             <div class="row">
                 <div class="logo">
-                    <a href="index.html">
-                        <img src="img/logo.png" alt="логотип">
+                    <a href=" <?= home_url(); ?> ">
+
+                        <!--  вывод логотипа через визуальные возможности админки вордпресса -->
+                        <!-- <img src="img/logo.png" alt="логотип"> -->
+                        <?php the_custom_logo(); ?>
                     </a>
                 </div>
                 <ul>
